5fe4d89594b8db1d04dd06e40f4ba0e33baa0913
aishwa8141 <a.aish1997@gmail.com>
2020-05-16 20:42:10 +0530

    added logback.xml with mdc

--- service-seed-without-router/all-actors/src/main/java/org/sunbird/BaseActor.java
+++ service-seed-without-router/all-actors/src/main/java/org/sunbird/BaseActor.java
@@ -1,66 +1,79 @@
 package org.sunbird;
 
 import akka.actor.UntypedAbstractActor;
-import org.apache.log4j.Logger;
+import akka.event.DiagnosticLoggingAdapter;
+import akka.event.Logging;
 import org.sunbird.message.IResponseMessage;
 import org.sunbird.message.Localizer;
 import org.sunbird.message.ResponseCode;
 import org.sunbird.request.Request;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * @author Amit Kumar
  */
 public abstract class BaseActor extends UntypedAbstractActor {
 
-    private Logger logger = Logger.getLogger(BaseActor.class);
+    final DiagnosticLoggingAdapter log = Logging.getLogger(this);
+
     public abstract void onReceive(Request request) throws Throwable;
+
     protected Localizer localizer = Localizer.getInstance();
 
     @Override
     public void onReceive(Object message) throws Throwable {
+        Map<String, Object> mdc;
+        mdc = new HashMap<>();
+        mdc.put("requestId", UUID.randomUUID().toString());
+        log.setMDC(mdc);
         if (message instanceof Request) {
             Request request = (Request) message;
             String operation = request.getOperation();
-            logger.info("BaseActor:onReceive called for operation:" + operation);
             try {
-                logger.info(String.format("%s:%s:method started at %s",this.getClass().getSimpleName(),operation,System.currentTimeMillis()));
+                log.info(String.format("%s:%s:method started at %s", this.getClass().getSimpleName(), operation, System.currentTimeMillis()));
                 onReceive(request);
-                logger.info(String.format("%s:%s:method ended at %s",this.getClass().getSimpleName(),operation,System.currentTimeMillis()));
+                log.info(String.format("%s:%s:method ended at %s", this.getClass().getSimpleName(), operation, System.currentTimeMillis()));
             } catch (Exception e) {
                 onReceiveException(operation, e);
+            } finally {
+                log.clearMDC();
             }
         } else {
-            logger.info("BaseActor: onReceive called with invalid type of request.");
+            log.info("BaseActor: onReceive called with invalid type of request.");
         }
     }
 
     /**
      * this method will handle the exception
+     *
      * @param callerName
      * @param exception
      * @throws Exception
      */
     protected void onReceiveException(String callerName, Exception exception) throws Exception {
-        logger.error("Exception in message processing for: " + callerName + " :: message: " + exception.getMessage(), exception);
+//        logger.error("Exception in message processing for: " + callerName + " :: message: " + exception.getMessage(), exception);
         sender().tell(exception, self());
     }
 
 
     /**
      * this message will handle the unsupported actor operation
+     *
      * @param callerName
      */
     protected void onReceiveUnsupportedMessage(String callerName) {
-        logger.info(callerName + ": unsupported operation");
+//        logger.info(callerName + ": unsupported operation");
         /**
          * TODO Need to replace null reference from getLocalized method and replace with requested local.
          */
         BaseException exception =
                 new ActorServiceException.InvalidOperationName(
                         IResponseMessage.INVALID_OPERATION_NAME,
-                        getLocalizedMessage(IResponseMessage.INVALID_OPERATION_NAME,null),
+                        getLocalizedMessage(IResponseMessage.INVALID_OPERATION_NAME, null),
                         ResponseCode.CLIENT_ERROR.getCode());
         sender().tell(exception, self());
     }
@@ -68,12 +81,13 @@ public abstract class BaseActor extends UntypedAbstractActor {
 
     /**
      * this is method is used get message in different different locales
+     *
      * @param key
      * @param locale
      * @return
      */
 
-    protected String getLocalizedMessage(String key, Locale locale){
+    protected String getLocalizedMessage(String key, Locale locale) {
         return localizer.getMessage(key, locale);
     }
 
@@ -92,7 +106,7 @@ public abstract class BaseActor extends UntypedAbstractActor {
      * @param tag
      */
     public void startTrace(String tag) {
-        logger.info(String.format("%s:%s:started at %s", this.getClass().getSimpleName(), tag, getTimeStamp()));
+//        logger.info(String.format("%s:%s:started at %s", this.getClass().getSimpleName(), tag, getTimeStamp()));
     }
 
     /**
@@ -101,6 +115,6 @@ public abstract class BaseActor extends UntypedAbstractActor {
      * @param tag
      */
     public void endTrace(String tag) {
-        logger.info(String.format("%s:%s:ended at %s", this.getClass().getSimpleName(), tag, getTimeStamp()));
+//        logger.info(String.format("%s:%s:ended at %s", this.getClass().getSimpleName(), tag, getTimeStamp()));
     }
 }
--- service-seed-without-router/all-actors/src/main/java/org/sunbird/HealthActor.java
+++ service-seed-without-router/all-actors/src/main/java/org/sunbird/HealthActor.java
@@ -4,17 +4,35 @@ import org.sunbird.actor.core.ActorConfig;
 import org.sunbird.request.Request;
 import org.sunbird.response.Response;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 @ActorConfig(
         tasks = {"health"},
         dispatcher = "",
         asyncTasks = {}
 )
 public class HealthActor extends BaseActor {
+
     @Override
     public void onReceive(Request request) throws Throwable {
-        Response response = new Response();
-        response.put("Response",request.getRequest());
-        response.put("healthy",true);
-        sender().tell(response,self());
+        try {
+            String uuid = UUID.randomUUID().toString();
+            Map<String, Object> mdc;
+            mdc = new HashMap<>();
+            mdc.put("requestId",uuid );
+            log.setMDC(mdc);
+            log.info("onReceive method call started {}", request.toString());
+
+            Response response = new Response();
+            response.put("Response",request.getRequest());
+            response.put("healthy",true);
+            log.info("onReceive method call End ");
+            sender().tell(response,self());
+        } finally {
+            log.clearMDC();
+        }
+
     }
 }
